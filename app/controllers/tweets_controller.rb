f19dadb046395ae39082c7538b749c49148ab620
Arturo Castello <arturo@playjugo.com>
2015-02-21 15:39:33 -0500

    Added flash message for a new tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,6 +7,8 @@ class TweetsController < ApplicationController
 		@tweet = Tweet.new(tweet_params)
 		@tweet.save
 
+		flash[:success] = "You have created a new tweet"
+
 		redirect_to new_tweet_path
 	end
 
